a3577fcff09b491c3f3949c4a99ff3b4061f301a
Luiz Gabriel <luizgabriel.info@gmail.com>
2017-12-14 23:13:43 -0300

    Update clear_cache.sh

--- .scripts/clear_cache.sh
+++ .scripts/clear_cache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf /srv/shiny-server/spatial-parties/static/cache/*.gz > /var/log/codedeploy/spatial2_clear_cache.log 2>&1
+rm -rf /srv/shiny-server/spatial2/static/cache/*.gz > /var/log/codedeploy/spatial2_clear_cache.log 2>&1
