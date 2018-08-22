be34d6f9f561df32e49ea8b56a84ffd06d16cfe7
RCCSilva <rafael.coelho.x@gmail.com>
2018-08-22 18:01:39 -0300

    template para dep. estadual e nomes

--- data/build_party_template.R
+++ data/build_party_template.R
@@ -2,34 +2,17 @@ rm(list = ls())
 
 library(tidyverse)
 
-get_candidatos <- function(ano, cargo){
-  url_base <- "http://cepesp.io/api/consulta/candidatos"
-  
-  params <- list(
-    `cargo` = cargo,
-    `ano`   = ano
-  )
-  
-  httr::GET(url_base, query = params) %>% 
-    httr::content(type = "text/plain", encoding = "UTF-8") %>% 
-    readr::read_csv(col_types = cols_only(ANO_ELEICAO                    = col_integer(),
-                                          SIGLA_UF                       = col_character(),
-                                          NUM_TURNO                      = col_integer(),
-                                          CODIGO_CARGO                   = col_integer(),
-                                          NUMERO_PARTIDO                 = col_integer(),
-                                          SIGLA_PARTIDO                  = col_character()),
-                    locale = locale(encoding = "UTF-8"))
-}
+args <- list(position = c(1,3,5,6,7))
 
-# 1. Carregando os Bancos -------------------------------------------------
-
-cargos <- c(1,3,5,6)
-
-anos <- c(1998,2002,2006,2010,2014)
-
-args <- expand.grid(ano = anos, cargo = cargos)
-
-candidatos_ls <- pmap(args,get_candidatos)
+candidatos_ls <- pmap(args, 
+                      cepespR::get_candidates,
+                      columns_list = list("ANO_ELEICAO",
+                                          "NUM_TURNO",
+                                          'SIGLA_UF',
+                                          'CODIGO_CARGO',
+                                          'SIGLA_PARTIDO',
+                                          'NOME_CANDIDATO',
+                                          'NUMERO_CANDIDATO'))
 
 candidatos_df <- bind_rows(candidatos_ls)
 
