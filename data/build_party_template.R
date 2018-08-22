851430fa2b0c37f610d6c4033b4561f2438e0d18
Rafael Coelho <RCCSilva@users.noreply.github.com>
2018-08-22 19:50:16 -0300

    new template data (DEP EST. e NUMERO CAND)

--- data/build_party_template.R
+++ data/build_party_template.R
@@ -2,24 +2,37 @@ rm(list = ls())
 
 library(tidyverse)
 
-args <- list(position = c(1,3,5,6,7))
+# args <- list(position = c(1,3,5,6,7))
+#
+# candidatos_ls <- pmap(args, 
+#                       cepespR::get_candidates,
+#                       year = "1998,2002,2006,2010,2014",
+#                       columns_list = list("ANO_ELEICAO",
+#                                           "NUM_TURNO",
+#                                           'SIGLA_UF',
+#                                           'CODIGO_CARGO',
+#                                           'SIGLA_PARTIDO',
+#                                           'NOME_CANDIDATO',
+#                                           'NUMERO_CANDIDATO'))
+# 
+# candidatos_ls <- map(candidatos_ls,
+#                      mutate, 
+#                      NUM_TITULO_ELEITORAL_CANDIDATO = as.double(NUM_TITULO_ELEITORAL_CANDIDATO),
+#                      DATA_NASCIMENTO                = as.character(DATA_NASCIMENTO))
+# 
+# candidatos_df <- bind_rows(candidatos_ls)
+# 
+# write_rds(candidatos_df, "data/complete_data/candidatos.rds")
 
-candidatos_ls <- pmap(args, 
-                      cepespR::get_candidates,
-                      columns_list = list("ANO_ELEICAO",
-                                          "NUM_TURNO",
-                                          'SIGLA_UF',
-                                          'CODIGO_CARGO',
-                                          'SIGLA_PARTIDO',
-                                          'NOME_CANDIDATO',
-                                          'NUMERO_CANDIDATO'))
+candidatos_df <- read_rds('data/complete_data/candidatos.rds')
 
-candidatos_df <- bind_rows(candidatos_ls)
+template <- candidatos_df %>% 
+  group_by(ANO_ELEICAO, NUM_TURNO, SIGLA_UF, CODIGO_CARGO, SIGLA_PARTIDO) %>% 
+  summarise(LISTA_NOMES  = list(NOME_CANDIDATO),
+            LISTA_NUMERO = list(NUMERO_CANDIDATO))
 
-candidatos_df$PARTIDOS <- PARTIDOS
+for(i in seq_along(template$LISTA_NUMERO)){
+  names(template$LISTA_NUMERO[[i]]) <- template$LISTA_NOMES[[i]]
+}
 
-candidatos_df <- candidatos_df %>% 
-  group_by(ANO_ELEICAO, NUM_TURNO, SIGLA_UF, CODIGO_CARGO) %>% 
-  summarise(PARTIDOS = list(SIGLA_PARTIDO))
-
-write_rds(candidatos_df, "data/party_template.rds")
+write_rds(template, "data/party_template.rds")
