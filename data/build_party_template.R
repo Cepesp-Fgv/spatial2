0beb5f9c11f6805aa81e6e00f0efbc8e8bffaeff
RCCSilva <rafael.coelho.x@gmail.com>
2018-09-10 11:04:02 -0300

    SIGLA_PARTIDO entre (

--- data/build_party_template.R
+++ data/build_party_template.R
@@ -1,10 +1,10 @@
 rm(list = ls())
 
 library(tidyverse)
-
+# 
 # args <- list(position = c(1,3,5,6,7))
-#
-# candidatos_ls <- pmap(args, 
+# 
+# candidatos_ls <- pmap(args,
 #                       cepespR::get_candidates,
 #                       year = "1998,2002,2006,2010,2014",
 #                       columns_list = list("ANO_ELEICAO",
@@ -16,7 +16,7 @@ library(tidyverse)
 #                                           'NUMERO_CANDIDATO'))
 # 
 # candidatos_ls <- map(candidatos_ls,
-#                      mutate, 
+#                      mutate,
 #                      NUM_TITULO_ELEITORAL_CANDIDATO = as.double(NUM_TITULO_ELEITORAL_CANDIDATO),
 #                      DATA_NASCIMENTO                = as.character(DATA_NASCIMENTO))
 # 
@@ -36,6 +36,7 @@ template <- candidatos_df %>%
                             1,
                             2)) %>% 
   group_by(ANO_ELEICAO, NUM_TURNO, SIGLA_UF, CODIGO_CARGO, SIGLA_PARTIDO, RESULTADO) %>% 
+  mutate(NOME_URNA_CANDIDATO = paste0(NOME_URNA_CANDIDATO, " (", SIGLA_PARTIDO,")")) %>% 
   summarise(LISTA_NOMES  = list(NOME_URNA_CANDIDATO),
             LISTA_NUMERO = list(NUMERO_CANDIDATO))
 
