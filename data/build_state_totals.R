bcf366c8db7298d19fe971e3b786e9329a343023
JonnyPhillips <jonnyphillips@gmail.com>
2019-03-09 15:37:26 -0300

    Update state_totals

--- data/build_state_totals.R
+++ data/build_state_totals.R
@@ -4,22 +4,29 @@ suppressWarnings(dir.create("data/state_totals"))
 
 # 1. Download -------------------------------------------------------------
 
-anos <- seq(1998,2014, by = 4)
-cargos <- c(1,3,5,6,7)
+anos <- seq(1998,2018, by = 4)
+cargos <- c(3,5,6,7)
 args <- expand.grid(year = anos, position = cargos)
 
-vars_state <-  list("UF", "QTDE_VOTOS", "ANO_ELEICAO")
+#vars_state <-  list("UF", "QTDE_VOTOS", "ANO_ELEICAO")
 
-state_ls <- purrr::pmap(args, cepespR::get_votes,
-                        regional_aggregation = 2,
-                        columns_list = vars_state)
+#state_ls <- purrr::pmap(args, cepespR::get_votes,
+#                        regional_aggregation = "State",
+#                        columns_list = vars_state)
 
-state_temp <- purrr::map(state_ls, dplyr::select, UF, QTDE_VOTOS)
+state_ls <- purrr::pmap(args, cepespR::get_elections, 
+                        regional_aggregation = "State", 
+                        political_aggregation="Consolidated",
+                        dev=T)
 
-state_temp <- purrr::map(state_temp, dplyr::rename, Tot_State = QTDE_VOTOS)
+state_temp <- purrr::map(state_ls, dplyr::select, UF, NUM_TURNO,QTD_COMPARECIMENTO)
+
+state_temp <- purrr::map(state_temp, dplyr::rename, Tot_State = QTD_COMPARECIMENTO)
 
 # 2. Save -----------------------------------------------------------------
+
 
 for(i in seq_along(state_temp)){
   readr::write_rds(state_temp[[i]], paste0("data/state_totals/",args$year[[i]], "_", args$position[[i]], ".rds"))
 }
+
