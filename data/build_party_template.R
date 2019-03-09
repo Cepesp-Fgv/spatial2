8ce78914315dd6b198d6c114596b9be0d93c8bb9
JonnyPhillips <jonnyphillips@gmail.com>
2019-03-09 15:14:33 -0300

    Update party_template.rds

--- data/build_party_template.R
+++ data/build_party_template.R
@@ -1,34 +1,41 @@
 rm(list = ls())
 
 library(tidyverse)
-# 
-# args <- list(position = c(1,3,5,6,7))
-# 
-# candidatos_ls <- pmap(args,
-#                       cepespR::get_candidates,
-#                       year = "1998,2002,2006,2010,2014",
-#                       columns_list = list("ANO_ELEICAO",
-#                                           "NUM_TURNO",
-#                                           'SIGLA_UF',
-#                                           'CODIGO_CARGO',
-#                                           'SIGLA_PARTIDO',
-#                                           'NOME_CANDIDATO',
-#                                           'NUMERO_CANDIDATO'))
-# 
-# candidatos_ls <- map(candidatos_ls,
-#                      mutate,
-#                      NUM_TITULO_ELEITORAL_CANDIDATO = as.double(NUM_TITULO_ELEITORAL_CANDIDATO),
-#                      DATA_NASCIMENTO                = as.character(DATA_NASCIMENTO))
-# 
-# candidatos_df <- bind_rows(candidatos_ls)
-# 
-# write_rds(candidatos_df, "data/complete_data/candidatos.rds")
+ 
+ args <- expand.grid(position = c(1,3,5,6,7),
+              year=c("1998","2002","2006","2010","2014","2018"))
+ 
+ candidatos_ls <- pmap(args,
+                       cepespR::get_candidates,
+                       columns_list = list("ANO_ELEICAO",
+                                           "NUM_TURNO",
+                                           'SIGLA_UF',
+                                           'CODIGO_CARGO',
+                                           'SIGLA_PARTIDO',
+                                           'NOME_CANDIDATO',
+                                           'NOME_URNA_CANDIDATO',
+                                           'NUMERO_CANDIDATO',
+                                           'NUM_TITULO_ELEITORAL_CANDIDATO',
+                                           'DATA_NASCIMENTO',
+                                           'DES_SITUACAO_CANDIDATURA',
+                                           'DESC_SIT_TOT_TURNO'))
+ 
+ 
+ candidatos_ls <- map(candidatos_ls,
+                      mutate,
+                      NUM_TITULO_ELEITORAL_CANDIDATO = as.double(NUM_TITULO_ELEITORAL_CANDIDATO),
+                      DATA_NASCIMENTO                = as.character(DATA_NASCIMENTO))
+ 
+ candidatos_df <- bind_rows(candidatos_ls)
+ 
+ write_rds(candidatos_df, "C:/Users/Jonny/Google Drive/Academic/FGV-SP/shiny/New_apps_2018/spatial2-2019/data/candidatos.rds")
 
-candidatos_df <- read_rds('data/complete_data/candidatos.rds')
+candidatos_df <- read_rds('C:/Users/Jonny/Google Drive/Academic/FGV-SP/shiny/New_apps_2018/spatial2-2019/data/candidatos.rds')
 
 template <- candidatos_df %>% 
   filter(DES_SITUACAO_CANDIDATURA %in% c('DEFERIDO',
-                                         'DEFERIDO COM RECURSO')) %>% 
+                                         'DEFERIDO COM RECURSO',
+                                         'APTO')) %>% 
   mutate(RESULTADO = ifelse(DESC_SIT_TOT_TURNO %in% c('ELEITO',
                                                       'ELEITO POR QP',
                                                       'ELEITO POR MÉDIA',
@@ -44,4 +51,4 @@ for(i in seq_along(template$LISTA_NUMERO)){
   names(template$LISTA_NUMERO[[i]]) <- template$LISTA_NOMES[[i]]
 }
 
-write_rds(template, "data/party_template.rds")
+write_rds(template, "C:/Users/Jonny/Google Drive/Academic/FGV-SP/shiny/New_apps_2018/spatial2-2019/data/party_template.rds")
